a0e805900bfe9cec7db9bb247538e87a2479a4e3
iLuvYouMegaHomo <megaluigi2804@gmail.com>
2022-07-11 13:08:33 +0700

    uhhhh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home.js'
+import NavBar from './components/NavBar.js'
+import Test from "./components/Test.js";
+import "./App.css";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 function App() {
   return (
     <div className="App">
-     <WalletMultiButton/>
+      <WalletMultiButton />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
     </div>
   );
 }
