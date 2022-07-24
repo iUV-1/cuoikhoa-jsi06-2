31a760dcb91916ee89ba0657a2db73dc9d43ee76
iLuvYouMegaHomo <megaluigi2804@gmail.com>
2022-07-24 11:04:24 +0700

    NavBer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Link as NextUILink, Spacer, Container, Row } from "@nextui-org/react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import mainLogo from "../img/mainLogo.png";
 import { Authorized } from "@solana/web3.js";
@@ -9,15 +8,16 @@ import "./style/NavBar.css";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const NavBar = () => {
+  console.log(window.location.pathname)
   return (
     <>
       <div className="NavBarContainer">
-        <RouterLink to={""} className="NavBarHomeLink">
+        <RouterLink to={""} className={`NavBarHomeLink ${window.location.pathname === "/" ? "NavBarLinkCurrent" : ""} ` }>
           <img src={mainLogo} alt="logo" className="NavBarHomeIMG" />
           Home
         </RouterLink>
 
-        <RouterLink to={"test"} className="NavBarLink">
+        <RouterLink to={"test"} className={`NavBarLink ${window.location.pathname === "/test" ? "NavBarLinkCurrent" : ""}`}>
           test
         </RouterLink>
         <WalletMultiButton />
