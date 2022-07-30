5a728669ca3f9fc64a8053a3383ac98eb230f0ed
Nhanhaiho <hainhan2335@gmail.com>
2022-07-30 22:18:22 +0700

    change sth in uploadfile

--- src/components/CreatePage/UpLoadFiles.js
+++ src/components/CreatePage/UpLoadFiles.js
@@ -1,25 +1,20 @@
 import React from 'react'
 // import { AiFillPicture } from 'react-icons/fa';
 import './UpLoadFiles.css'
-
+import { FaFileImage } from "react-icons/fa"
 
 const UpLoadBox = () => {
-    const box = {
-        width: "344px",
-        height: "251px",
-        border: "3px dashed rgb(204,204,204)",
-        cursor: "pointer",
-        borderRadius: "10px",
-        marginBottom:"10px"
-    }
+ 
     return <div className="UpLoadContainer">
         <label style={{fontSize:"16px",fontWeight:"600",marginBottom:"10px"}}>
             Image, Video, Audio, or 3D Model *
         </label>
         <span className="UpLoadExamples">File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 100 MB</span>
-        <div style={box}>
+        <div  className ="box">
             {/* <AiFillPicture/> */}
-            
+            <input type="file"/>
+            <FaFileImage className="icon-upload" />
+          
         </div>
     </div>
 
