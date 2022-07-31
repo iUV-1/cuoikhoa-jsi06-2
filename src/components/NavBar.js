839e237974bcdd2740df01840b16f852d5b03462
iLuvYouMegaHomo <megaluigi2804@gmail.com>
2022-07-31 10:33:14 +0700

    fixed NavBar a bit more

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,26 +8,47 @@ import "./style/NavBar.css";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const NavBar = () => {
-  console.log(window.location.pathname)
+  console.log(window.location.pathname);
   // apparently putting this here automatically fix the reload issue?
   const location = useLocation();
 
   return (
     <>
       <div className="NavBarContainer">
-        <RouterLink to={""} className={`NavBarHomeLink ${window.location.pathname === "/" ? "NavBarLinkCurrent" : ""} ` }>
+        <div style={{display : "flex"}}>
+        <RouterLink
+          to={""}
+          className={`NavBarHomeLink ${
+            window.location.pathname === "/" ? "NavBarLinkCurrent" : ""
+          } `}
+        >
           <img src={mainLogo} alt="logo" className="NavBarHomeIMG" />
           Home
         </RouterLink>
 
-        <RouterLink to={"test"} className={`NavBarLink ${window.location.pathname === "/test" ? "NavBarLinkCurrent" : ""}`}>
+        <RouterLink
+          to={"test"}
+          className={`NavBarLink ${
+            window.location.pathname === "/test" ? "NavBarLinkCurrent" : ""
+          }`}
+        >
           test
         </RouterLink>
 
-        <RouterLink to={"CreateItems"} className={`NavBarLink ${window.location.pathname === "/CreateItems" ? "NavBarLinkCurrent" : ""}`}>
-          CreateItems
+        <RouterLink
+          to={"CreateItems"}
+          className={`NavBarLink ${
+            window.location.pathname === "/CreateItems"
+              ? "NavBarLinkCurrent"
+              : ""
+          }`}
+        >
+          Create
         </RouterLink>
-        <WalletMultiButton />
+        </div>
+
+        <WalletMultiButton style={{backgroundColor: "black"}} />
+
       </div>
     </>
   );
